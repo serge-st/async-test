3860a9758ef517324f8779230af0f0cae6fb0c93
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-03-01 12:46:02 +0200

    feat: test parallel execution vs defaul

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 import { performance } from 'perf_hooks'
 import { Worker } from 'worker_threads';
+import { SERVER_PORT } from './constants';
+
 const app = express()
-const port = 3000
 
 function fibonacci(n: number): number {
     if (n <= 1) return 1;
@@ -23,29 +24,33 @@ function fibonacciInWorker(n: number) {
 }
 
 app.get('/worker', async (req, res) => {
-    const start = performance.now();
-    await fibonacciInWorker(42)
+    const count = req.query.count || 1
+    console.log(`Processing worker request ${count}`);
+    performance.mark('start')
+    const result = await fibonacciInWorker(42)
     await new Promise((resolve) => setTimeout(resolve, 2000))
-    const end = performance.now();
-    console.log(`Request took ${end - start}ms`)
-    console.log('-------------------')
-    res.send('Hello World!')
+    performance.mark('end')
+    const { duration } = performance.measure('Worker Duration', 'start', 'end');
+    const isTrueResult = result === 433494437;
+    console.log(`Regular ${count} Duration: ${duration}, result is ${isTrueResult ? 'correct' : 'incorrect'}`)
+    console.log('=====================')
+    res.send(`Worker is done: ${result}`);
 });
 
-app.get('/', async (req, res) => {
-    const start = performance.now();
-    fibonacci(42)
+app.get('/regular', async (req, res) => {
+    const count = req.query.count || 1
+    console.log(`Processing regular request ${count}`);
+    performance.mark('start')
+    const result = fibonacci(42)
     await new Promise((resolve) => setTimeout(resolve, 2000))
-    const end = performance.now();
-    console.log(`Request took ${end - start}ms`)
-    console.log('-------------------')
-    res.send('Hello World!')
+    performance.mark('end')
+    const { duration } = performance.measure('Regular Duration', 'start', 'end');
+    const isTrueResult = result === 433494437;
+    console.log(`Regular ${count} Duration: ${duration}, result is ${isTrueResult ? 'correct' : 'incorrect'}`)
+    console.log('=====================')
+    res.send(`Regular is done: ${result}`);
 })
 
-app.get('/line', async (req, res) => {
-    res.send(`=====================\n=====================\n=====================\n`)
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(SERVER_PORT, () => {
+    console.log(`Server app listening on port ${SERVER_PORT}`)
 })
---
+++ src/utils/requestTester.ts
@@ -0,0 +1,61 @@
+import express from 'express'
+import { performance } from 'perf_hooks'
+import { SERVER_PORT } from '../constants';
+
+const testerApp = express()
+const testerPort = 4224
+const requestCount = 10;
+
+testerApp.get('/test-worker', async (req, res) => {
+    console.log('Starting to test worker')
+    performance.mark('single_start')
+    console.log('Sending a single request to worker')
+    await fetch(`http://localhost:${SERVER_PORT}/worker`)
+    console.log('Single request to worker is done')
+    performance.mark('single_end')
+    const singleRequestResult = performance.measure('Single Worker Request Duration', 'single_start', 'single_end');
+    console.log(`Single Worker Request Duration: ${singleRequestResult.duration}`)
+
+    console.log(`Sending ${requestCount} parallel requests to worker`)
+    performance.mark('parallel_start')
+    const requests = Array.from({ length: requestCount }, (_, i) => fetch(`http://localhost:${SERVER_PORT}/worker?count=${i + 1}`))
+    await Promise.all(requests);
+    console.log(`Parallel requests to worker are done`)
+    performance.mark('parallel_end')
+    const { duration: totalDuration } = performance.measure('Parallel Worker Request Duration', 'parallel_start', 'parallel_end');
+    const averageDuration = totalDuration / requestCount;
+    console.log(`Parallel Worker Request Average Duration: ${averageDuration}`);
+    console.log(`Parallel Worker Request Total Duration: ${totalDuration}`);
+
+    console.log('=====================')
+    res.send('done\n');
+});
+
+testerApp.get('/test-regular', async (req, res) => {
+    console.log('Starting to test regular')
+    performance.mark('single_start')
+    console.log('Sending a single request to regular')
+    await fetch(`http://localhost:${SERVER_PORT}/regular`)
+    console.log('Single request to regular is done')
+    performance.mark('single_end')
+    const singleRequestResult = performance.measure('Single Regular Request Duration', 'single_start', 'single_end');
+    console.log(`Single Regular Request Duration: ${singleRequestResult.duration}`)
+
+    console.log(`Sending ${requestCount} parallel requests to regular`)
+    performance.mark('parallel_start')
+    const requests = Array.from({ length: requestCount }, (_, i) => fetch(`http://localhost:${SERVER_PORT}/regular?count=${i + 1}`))
+    await Promise.all(requests)
+    console.log(`Parallel requests to regular are done`)
+    performance.mark('parallel_end')
+    const { duration: totalDuration } = performance.measure('Parallel Regular Request Duration', 'parallel_start', 'parallel_end');
+    const averageDuration = totalDuration / requestCount;
+    console.log(`Parallel Regular Request Average Duration: ${averageDuration}`);
+    console.log(`Parallel Regular Request Total Duration: ${totalDuration}`);
+
+    console.log('=====================')
+    res.send('done\n');
+});
+
+testerApp.listen(testerPort, () => {
+    console.log(`Tester app listening on port ${testerPort}`)
+});
